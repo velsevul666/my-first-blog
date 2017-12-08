ccb52abd360980b8a862a7e249fdd2ed3127ff84
1 <karinka7410@gmail.com>
2017-12-08 17:37:44 +0300

    velsevul666

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hnc*ady7t_t-3jgu&d^$oz09#(g3nina=19*-dhh!0h^-@)vf^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','1.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','velsevul666.pythonanywhere.com']
 
 
 # Application definition
