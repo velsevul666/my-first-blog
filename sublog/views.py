16eb8a8a5ff8972d5dac19a2f9fa3d2589b98231
1 <karinka7410@gmail.com>
2017-12-16 20:57:31 +0300

    popravka

--- sublog/views.py
+++ sublog/views.py
@@ -8,4 +8,4 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'sublog/post_detail.html', {'post': post})
